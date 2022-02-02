914906ced62c64bc37b5764f5e129ba79d5c9e24
M.Haris Hanif <harishanif970@gmail.com>
2022-02-02 12:16:03 +0500

    Few Lines Added

--- test.r
+++ test.r
@@ -1,2 +1,9 @@
 # This is my first file on github 
 print("This is my First File on github")
+
+name = "M.Haris"
+Version = "RStudio "
+ 
+print(name)
+print("This changes is writen in Rstudio")
+print("It's all about practise")
